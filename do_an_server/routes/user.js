5b281a4d00e92f737e7fa265715745d1deac7cdb
anhthu2503 <anhthu25032001@gmail.com>
2020-12-21 19:36:30 +0700

    push code php and react

---
+++ do_an_server/routes/user.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({'xu_ly': 'thông tin user'});
+});
+
+router.get('/:id_user', (req, res) => {
+    res.json({'xu_ly': 'thông tin user ' + req.params.id_user});
+});
+
+router.post('/', (req, res) => {
+    res.json({
+        'xu_ly': 'thêm user mới',
+        data_send: req.body
+    });
+});
+
+module.exports = router;
+
+//server nhan duoc 3 dang: querry, param(id), body
+//tao route cho data
